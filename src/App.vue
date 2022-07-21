6ce5d8f404a171aec8c9f1715fd571fa5d09f44c
MrGians <gianlumura97@gmail.com>
2022-07-21 16:16:20 +0200

    test 2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <Title />
+    <MyTitle />
     <img alt="Vue logo" src="./assets/logo.png" />
   </div>
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
-import Title from "./components/Title.vue";
+import MyTitle from "./components/MyTitle.vue";
 export default {
   name: "App",
   components: {
-    Title,
+    MyTitle,
   },
 };
 </script>
